e3cf2aa12d7289e18f08af9602a16a7976b579c8
iditmer <90423241+iditmer@users.noreply.github.com>
2026-04-28 19:38:19 -0800

    Add basic pytest project setup

---
+++ tests/test_base_functions.py
@@ -0,0 +1,9 @@
+from manifold_terrain import base_functions
+
+def test_rational_slope_height_at_center():
+    
+    f = base_functions.rational_slope(45.0, 12.0, 1.0)
+    assert f(45.0, 0.0) == 6.0
+
+    f = base_functions.rational_slope(65.0, -10.0, 1.0)
+    assert f(65.0, 12.0) == 7.0
