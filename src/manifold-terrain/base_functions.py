cce9f93089ca2b4fe4d2e0a2d5a0e48bdc4b5f99
iditmer <90423241+iditmer@users.noreply.github.com>
2026-04-28 18:55:25 -0800

    Update parameters to reflect using slope at curve center

--- src/manifold-terrain/base_functions.py
+++ src/manifold-terrain/base_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def rational_slope(center, height, width):
+def rational_slope(center, height, slope_at_center):
     def slope(x, y):
-        return y + (0.5 * height) * (1 - ((x - center) / width) / -np.sqrt(1 + ((x - center) / width) ** 2))
+        arg = (x - center) / (height / (2 * slope_at_center))
+        return y + (0.5 * height) * (1 + arg / np.sqrt(1 + arg ** 2))
     return slope
