d0a48f00af7e23f06fae58b283251f8893e62dee
iditmer <90423241+iditmer@users.noreply.github.com>
2026-04-30 12:51:30 -0800

    Update test to use pytest parameters

--- tests/test_base_functions.py
+++ tests/test_base_functions.py
@@ -1,9 +1,8 @@
+import pytest
 from manifold_terrain import base_functions
 
-def test_rational_slope_height_at_center():
-    
-    f = base_functions.rational_slope(45.0, 12.0, 1.0)
-    assert f(45.0) == 6.0
-
-    f = base_functions.rational_slope(65.0, -10.0, 1.0)
-    assert f(65.0) == -5.0
+@pytest.mark.parametrize("input, expected", [((45.0, 12.0, 1.0), 6.0), ((75.0, -8.0, 2.0), -4.0)])
+def test_rational_slope_height_at_center(input, expected):    
+    center, height, slope = input
+    f = base_functions.rational_slope(center, height, slope)
+    assert f(center) == expected
