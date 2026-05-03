f632505a1a38cac1bbbc9acc69afaf8b86306900
iditmer <90423241+iditmer@users.noreply.github.com>
2026-05-03 13:29:52 -0800

    Update irrational slope definition.
    Renamed from 'rational_slope' to 'irrational_slope' to reflect
    presense of radical in formula; restructure function definition
    to mirror lorentzian peak function in parameter parsing etc.

--- src/mterrain/functions.py
+++ src/mterrain/functions.py
@@ -3,98 +3,10 @@ from collections.abc import Callable
 from numpy.typing import NDArray
 from typing import Sequence
 
-def rational_slope(center: float | Sequence[float], height: float | Sequence[float], slope_at_center: float | Sequence[float]) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
-    """
-    Generate a rational function whose graph has a sigmoidal shape.
-
-    Parameters
-    ----------
-    center : float
-        Coordinate value for the center of the curve along the independent axis
-    height : float
-        The approximate distance from the bottom to top of the curve in the dependent direction; 
-        asymptotic behavior means the output will not have this exact height
-    slope_at_center : float
-        Real number describing instantaneous slope of curve at its center; 
-        impacts 'length' of the sigmoid - lower slope => more stretched along independent axis
-
-    Returns
-    -------
-    callable
-        A function that takes an array as input and returns an array of values on the curve as output
-    """
-
-    def slope(x: NDArray[np.float64]) -> NDArray[np.float64]:
-        """
-        Compute values along a sigmoidal curve whose parameters were previously specified.
-
-        Parameters
-        ----------
-        x : ndarray
-            Input array of coordinate values along independent axis
-
-        Returns
-        -------
-        ndarray
-            Output array of heights on curve
-        """
-
-        arg = (2 * slope_at_center / height) * (x - center)
-        return 0.5 * height * (1 + arg / np.sqrt(1 + arg ** 2))
-    
-    return slope
-
-def sum_of_rational_slopes(centers: Sequence[float], heights: Sequence[float], slope_at_centers: Sequence[float]) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
-    """
-    Generate a function that is the sum of rational slope functions.
-
-    Parameters
-    ----------
-    centers : float
-        Coordinate values for the centers of the component curves
-    heights : float
-        The approximate distance from the bottom to top of each component curve
-    slope_at_centers : float
-        Instantaneous slope of curves at their respective centers
-
-    Returns
-    -------
-    callable
-        A function that takes an array as input and returns an array of values on the curve as output
-    """
-
-    param_lens = set([len(centers), len(heights), len(slope_at_centers)])
-    if len(param_lens) > 1:
-        raise ValueError("An equal number of each parameter is required to define component curves.")
-    if 0 in param_lens:
-        raise ValueError("A non-zero number of each parameter is required to define component curves.")
-    
-    def slope(x: NDArray[np.float64]) -> NDArray[np.float64]:
-        """
-        Compute values along a sum of sigmoidal curves whose parameters were previously specified.
-
-        Parameters
-        ----------
-        x : ndarray
-            Input array of coordinate values along independent axis
-
-        Returns
-        -------
-        ndarray
-            Output array of heights on curve
-        """
-        output = np.zeros_like(x)
-        for (c, h, s) in zip(centers, heights, slope_at_centers):
-            arg = (2 * s / h) * (x - c)
-            output = output + 0.5 * h * (1 + arg / np.sqrt(1 + arg ** 2))
-        return output
-    
-    return slope
-
 def lorentzian_peak(height: float | Sequence[float], center: float | Sequence[float], width: float | Sequence[float]) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
     """
     Generate scaled Lorentzian function whose graph is a single peak; if parameters provided
-    as sequences, output function returns a sum of constituent Lorentzian functions.    
+    as sequences, output function returns a sum of constituent functions.    
 
     Parameters
     ----------
@@ -119,12 +31,12 @@ def lorentzian_peak(height: float | Sequence[float], center: float | Sequence[fl
     
     for h in height:
         if h == 0.0:
-            raise ValueError(f"Invalid peak parameter. Expects non-zero height. Input: {h}")
+            raise ValueError(f"Invalid curve parameter. Expects non-zero height. Input: {h}")
     for w in width:
         if w == 0.0:
-            raise ValueError(f"Invalid peak parameter. Expects non-zero width. Input: {w}")
+            raise ValueError(f"Invalid curve parameter. Expects non-zero width. Input: {w}")
         if w < 0.0:
-            raise ValueError(f"Invalid peak parameter. Expects positive width. Input: {w}")
+            raise ValueError(f"Invalid curve parameter. Expects positive width. Input: {w}")
     
     param_lens = set([len(height), len(center), len(width)])
     if 0 in param_lens:
@@ -152,3 +64,66 @@ def lorentzian_peak(height: float | Sequence[float], center: float | Sequence[fl
         return output
     
     return peak
+
+def irrational_slope(height: float | Sequence[float], center: float | Sequence[float], slope: float | Sequence[float]) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
+    """
+    Generate irrational function whose graph is a sigmoidal slope; if parameters provided
+    as sequences, output function returns a sum of constituent functions.    
+
+    Parameters
+    ----------
+    height : float
+        Approximate total height(s) spanned by curve(s) asymptotically
+    center : float
+        Coordinate value(s) for center(s) inflection point(s) of curve(s) along the independent axis
+    slope : float
+        Maximum slope(s) of the curve(s); occurs at center(s)
+
+    Returns
+    -------
+    callable
+        Computes heights on the resulting curve given an array of coordinate values
+    """
+    if isinstance(height, (int, float)):
+        height =  [height]
+    if isinstance(center, (int, float)):
+        center = [center]
+    if isinstance(slope, (int, float)):
+        slope = [slope]
+    
+    for h in height:
+        if h == 0.0:
+            raise ValueError(f"Invalid curve parameter. Expects non-zero height. Input: {h}")
+        if h < 0.0:
+            raise ValueError(f"Invalid curve parameter. Expects positive height. Input: {h}")
+    for s in slope:
+        if s == 0.0:
+            raise ValueError(f"Invalid curve parameter. Expects non-zero slope. Input: {s}")
+    
+    param_lens = set([len(height), len(center), len(slope)])
+    if 0 in param_lens:
+        raise ValueError("A non-zero number of each parameter is required to define component curves.")
+    if len(param_lens) > 1:
+        raise ValueError("An equal number of each parameter is required to define component curves.")
+        
+    def sloped_curve(x: NDArray[np.float64]) -> NDArray[np.float64]:
+        """
+        Computes values along irrational sigmoidal curve (or sum of curves)
+
+        Parameters
+        ----------
+        x : ndarray
+            Array of coordinate values along independent axis
+
+        Returns
+        -------
+        ndarray
+            Output array of heights on curve (or sum of curves)
+        """
+        output = np.zeros_like(x)
+        for (h, c, s) in zip(height, center, slope):
+            arg = (2 * s / h) * (x - c)
+            output = output + 0.5 * h * (1 + arg / np.sqrt(1 + arg ** 2))
+        return output
+    
+    return sloped_curve
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,59 +1,60 @@
 import pytest
 from mterrain import functions
 
-@pytest.mark.parametrize("input, expected", [ ((45.0, 12.0, 1.0), 6.0), ((45.0, -12.0, 1.0), -6.0) ])
-def test_rational_slope_height_at_center(input, expected):    
-    center, height, slope = input
-    f = functions.rational_slope(center, height, slope)
-    assert f(center) == expected
-
-@pytest.mark.parametrize("input, expected",[
-    (((50.0, 7.0, 1.0), [10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]),
-    [ 0.0133219887636046, 0.0235790131946497, 0.0523933649550353, 0.196495752718939, 3.5, 
-      6.80350424728106, 6.94760663504496, 6.97642098680535, 6.9866780112364 ]),
-    (((50.0, 7.0, -0.2), [10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]),
-    [ 6.70655067215766, 6.52322615314452, 6.13401843147407, 5.23648628424892, 3.5, 
-      1.76351371575108, 0.865981568525928, 0.476773846855483, 0.293449327842338 ]),
-    (((-12.0, 4.0, 1.2), [ -25.0, -20.0, -15.0, -10.0, -5.0, 0.0, 5.0, 10.0, 15.0 ]),
-     [ 0.0162366706282988, 0.0420391605247898, 0.251685447756925, 3.53644255919475, 
-       3.94561242937073, 3.9809845463555, 3.99045705023996, 3.9942853760559, 3.99620046011194 ]),
-    (((-12.0, -4.0, 1.2), [ -25.0, -20.0, -15.0, -10.0, -5.0, 0.0, 5.0, 10.0, 15.0 ]),
-     [ -3.9837633293717, -3.95796083947521, -3.74831455224308, -0.463557440805248, -0.0543875706292662, 
-       -0.0190154536444969, -0.00954294976004033, -0.00571462394410016, -0.00379953988806303 ]),
-])
-def test_rational_slope_values(input, expected):
-    center, height, slope = input[0]
-    f = functions.rational_slope(center, height, slope)
-    for i in range(len(input[1])):
-        assert f(input[1][i]) == pytest.approx(expected[i])
-
-def test_sum_of_rational_slope_values():
-    
-    x = [ -20.0, -10.0, 0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, ]    
-    y = [ 0.035332329819115604, 0.045990192574770106, 0.06302452667065472, 0.09356946177848846, 
-          0.1598642975653543, 0.3666903199551057, 1.8415165378250853, 10.372723109835936, 11.91812837082345, 
-          12.361451302850513, 13.394955961211272, 18.463302408041713, 23.52010023956195, 24.5043596643045 ]
-    f = functions.sum_of_rational_slopes( [ 45.0, 90.0 ], [ 12.0, 13.0 ], [ 1.2, 0.8 ] )
-
-    for i in range(len(x)):
-        assert f(x[i]) == pytest.approx(y[i])
-
 @pytest.mark.parametrize("parameters, message", [
                          ((0.0, 0.0, 1.0), "non-zero height"),
                          ((1.0, 0.0, 0.0), "non-zero width"),
                          ((1.0, 0.0, -1.0), "positive width"),
                          (([1.0, 1.0], [0.0], [2.0, 2.0]), "equal number"),
-                         (([1.0, 1.0], [0.0, 0.0], []), "non-zero number")])
+                         (([1.0, 1.0], [0.0, 0.0], []), "non-zero number")
+])
 def test_lorentzian_parameter_validation(parameters, message):
     with pytest.raises(ValueError) as err:
         f = functions.lorentzian_peak(parameters[0], parameters[1], parameters[2])
     assert message in str(err.value)
 
-@pytest.mark.parametrize("parameters", [ (25.0, -50.0, 12.0), (36.0, 25.0, 40.0) ])
-def test_lorentzian_peak(parameters):
+@pytest.mark.parametrize("parameters", [ 
+    (25.0, -50.0, 12.0), (36.0, 25.0, 40.0) 
+])
+def test_lorentzian_key_values(parameters):
+    
     height, center, width = parameters
     f = functions.lorentzian_peak(height, center, width)
+    
     assert f(center) == pytest.approx(height)
     assert f(center - 0.5 * width) == pytest.approx(0.5 * height)
     assert f(center + 0.5 * width) == pytest.approx(0.5 * height)
+
+@pytest.mark.parametrize("parameters, message", [
+                         ((0.0, 0.0, 1.0), "non-zero height"),
+                         ((-1.0, 0.0, 1.0), "positive height"),
+                         ((1.0, 0.0, 0.0), "non-zero slope"),
+                         (([1.0, 1.0], [0.0], [2.0, 2.0]), "equal number"),
+                         (([1.0, 1.0], [0.0, 0.0], []), "non-zero number")
+])
+def test_irrational_parameter_validation(parameters, message):
+    with pytest.raises(ValueError) as err:
+        f = functions.irrational_slope(parameters[0], parameters[1], parameters[2])
+    assert message in str(err.value)
+
+@pytest.mark.parametrize("parameters", [ 
+    (10.0, 0.0, 2.0), (10.0, 0.0, -1.2), 
+    (12.0, -8.0, 2.0), (12.0, -8.0, -1.2),
+    (8.0, 5.0, 1.0), (8.0, 5.0, -2.0)
+])
+def test_irrational_key_values(parameters):    
     
+    height, center, slope = parameters
+    f = functions.irrational_slope(height, center, slope)    
+    
+    assert f(center) == pytest.approx(0.5 * height)        
+    
+    if slope > 0:
+      assert f(-1e25) == pytest.approx(0.0)
+      assert f(1e25) == pytest.approx(height)
+    else: 
+      assert f(-1e25) == pytest.approx(height)
+      assert f(1e25) == pytest.approx(0.0)
+    
+    dy = f(center + 1e-6) - f(center - 1e-6)
+    assert (dy / 2e-6) == pytest.approx(slope)
